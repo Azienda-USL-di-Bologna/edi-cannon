78f46e4f3cb8e5eb94f175ae1de57db8734803f7
lazzarini <c.lazzarini@nextsw.it>
2024-10-01 17:58:17 +0200

    no message

--- internauta_data_manager.py
+++ internauta_data_manager.py
@@ -240,7 +240,7 @@ def upsert_doc_list_data(codice_azienda, json_data, conn, id_azienda):
             if json_data['id_message_shpeck'] is not None:
                 c.execute( qc.seleziona_id_mezzo,{
                     "mezzo": json_data['id_mezzo_ricezione'] })
-                id_mezzo = c.fetchone()[0];
+                id_mezzo = c.fetchone()["id"];
                 c.execute(qc.upsert_spedizione, {
                     "id_doc": id_doc,
                     "id_message": json_data['id_message_shpeck'],
@@ -256,7 +256,7 @@ def upsert_doc_list_data(codice_azienda, json_data, conn, id_azienda):
                     if related['tipo'] == 'MITTENTE':
                         c.execute(qc.seleziona_id_mezzo, {
                             "mezzo": "Babel"})
-                        id_mezzo = c.fetchone();
+                        id_mezzo = c.fetchone()["id"];
                         c.execute(qc.upsert_spedizione, {
                             "id_doc": id_doc,
                             "id_message": None,
