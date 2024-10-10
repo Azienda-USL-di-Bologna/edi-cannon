358870dd45c3842eee293aea2cef2ecb13570188
lazzarini <c.lazzarini@nextsw.it>
2024-10-10 14:43:20 +0200

    no message

--- queries_cannone.py
+++ queries_cannone.py
@@ -278,7 +278,7 @@ upsert_related_and_delete_the_others="""
             origine,  descrizione,data_inserimento
         ) 
         SELECT DISTINCT %(id_doc)s,  id_persona_inserente::integer, tipo::scripta.tipo_related, 
-            origine::scripta.origine_related,  descrizione::text, data_inserimento::timestamptz
+            origine::scripta.origine_related,  descrizione::text,  TO_TIMESTAMP(REPLACE(data_inserimento::text, 'T', ' '), 'YYYY-MM-DD HH24:MI:SS')::timestamptz
         FROM (
         VALUES  
             {values}
