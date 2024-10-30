2c137c02714e7e337c09581c5b0e01d96ec8fa22
lazzarini <c.lazzarini@nextsw.it>
2024-10-30 14:42:46 +0100

    no message

--- internauta_data_manager.py
+++ internauta_data_manager.py
@@ -452,14 +452,13 @@ def upsert_related(json_data, conn, id_azienda):
                     "id_esterno": related["id_esterno"]
                 })
             else:
-                if related['id_spedizione_pec'] is not None:
-                    c.execute(qc.upsert_spedizione, {
-                        "guid_doc": json_data["guid_documento"],
-                        "id_message": related['id_spedizione_pec'],
-                        "id_mezzo": id_mezzo,
-                        "indirizzo": related["indirizzo"],
-                        "id_esterno": related["id_esterno"]
-                    })
+                c.execute(qc.upsert_spedizione, {
+                    "guid_doc": json_data["guid_documento"],
+                    "id_message": related["id_spedizione_pec"],
+                    "id_mezzo": id_mezzo,
+                    "indirizzo": related["indirizzo"],
+                    "id_esterno": related["id_esterno"]
+                })
 
     else:
         c.execute(qc.delete_spedizione, {
