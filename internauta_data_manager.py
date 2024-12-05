799cc356a9e5fd3cf261f87319417c83c0186387
gusgus <f.gusella@nsi.it>
2024-12-05 14:25:25 +0100

    mezzo pec scritto PEC

--- internauta_data_manager.py
+++ internauta_data_manager.py
@@ -448,6 +448,8 @@ def upsert_related(json_data, conn, id_azienda):
                 related['mezzo'] = 'Raccomandata'
             if related['mezzo'] == 'Tel' or related['mezzo'] == 'telefono':
                 related['mezzo'] = 'Telefono'
+            if related['mezzo'] == 'PEC':
+                related['mezzo'] = 'Pec'
 
             log.info(f"questo è il mezzo che sto cercando: {related['mezzo']}" )
             c.execute(qc.seleziona_id_mezzo, {
