e231cc8e013332bcda36bad6931ced3090e2efd1
lazzarini <c.lazzarini@nextsw.it>
2025-01-17 16:45:18 +0100

    no message

--- internauta_data_manager.py
+++ internauta_data_manager.py
@@ -455,11 +455,11 @@ def upsert_related(json_data, conn, id_azienda):
     if json_data["related"] is not None and len(json_data['related']) > 0:
         for related in json_data['related']:
             # idStruttura può essere null solo perché nei vecchi attori non si riescie a fare il match con le strutture internuata
-            #faccio escape dei caratteri speciali e replace degli apostrofi
+            #faccio escape dei caratteri speciali e replace degli apostrofi, aggiungo il replace dei percento perchè psycopg è psico e sennò fa confusione coi parametri,
+            #gli altri caratteri non danno problemi
             descrizione = related['descrizione'].replace("'", "''").replace("%", "%%")
             indirizzo = related['indirizzo'].replace("'", "''").replace("%", "%%")
-            log.info(
-                f"mi da fastidio sto coso: {descrizione}")
+
             values_related = values_related + f"""(
                         {related['id_persona_inserente'] if related['id_persona_inserente'] is not None else 1}, 
                         {"'" + related['tipo'] + "'"},
