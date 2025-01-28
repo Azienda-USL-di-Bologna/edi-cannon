d8e9a5f58955911e4f251c9a6a72aff8bf0fd5cb
gusgus <f.gusella@nsi.it>
2025-01-28 10:17:28 +0100

    Aggiunta tradizione mezzo per Corriere -> Posta ordinaria

--- internauta_data_manager.py
+++ internauta_data_manager.py
@@ -495,7 +495,7 @@ def upsert_related(json_data, conn, id_azienda):
 
             if related['mezzo'] == 'Email' or related['mezzo'] is None:
                 related['mezzo'] = 'Mail'
-            if related['mezzo'] == 'Posta Ordinaria' or related['mezzo'] == 'P. Ordin.' or related['mezzo'] == 'Posta':
+            if related['mezzo'] == 'Posta Ordinaria' or related['mezzo'] == 'P. Ordin.' or related['mezzo'] == 'Posta' or related['mezzo'] == 'Corriere':
                 related['mezzo'] = 'Posta ordinaria'
             if related['mezzo'] == 'A Mano':
                 related['mezzo'] = 'A mano'
